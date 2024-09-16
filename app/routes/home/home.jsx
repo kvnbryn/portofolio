428049664e57bd20046004ae0e9bf481b1b3892e
kvnbryn <kvnbryank@gmail.com>
2024-09-16 13:57:18 +0800

    first commit

---
+++ app/routes/home/home.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useRef, useState } from 'react'
+import { Footer } from '~/components/footer'
+import config from '~/config.json'
+import { baseMeta } from '~/utils/meta'
+import styles from './home.module.css'
+import { Intro } from './intro'
+import { Profile } from './profile'
+
+// Prefetch draco decoader wasm
+export const links = () => {
+  return [
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_wasm_wrapper.js',
+      as: 'script',
+      type: 'text/javascript',
+      importance: 'low',
+    },
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_decoder.wasm',
+      as: 'fetch',
+      type: 'application/wasm',
+      importance: 'low',
+    },
+  ];
+};
+
+export const meta = () => {
+  return baseMeta({
+    title: 'Designer + Developer',
+    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+  });
+};
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, details]; // Definisikan array sections di sini
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      if (section.current) { // Pastikan section.current ada sebelum mengobservasi
+        sectionObserver.observe(section.current);
+      }
+    });
+
+    if (intro.current) { // Pastikan intro.current ada sebelum mengobservasi
+      indicatorObserver.observe(intro.current);
+    }
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, []); // Array dependensi kosong agar useEffect hanya berjalan sekali
+
+  return (
+    <div className={styles.home}>
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
